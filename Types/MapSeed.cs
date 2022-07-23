553d67063ae4b856305ca685a663c85ee86db860
sunyuanzhen <sunyuanzhen@gmail.com>
2022-07-23 17:22:22 +0800

    fix incorrect map seed

--- Types/MapSeed.cs
+++ Types/MapSeed.cs
@@ -7,68 +7,69 @@ namespace MapAssist.Types
     {
         private static readonly NLog.Logger _log = NLog.LogManager.GetCurrentClassLogger();
 
-        private BackgroundWorker BackgroundCalculator;
-        private uint GameSeedXor { get; set; } = 0;
+        // private BackgroundWorker BackgroundCalculator;
+        // private uint GameSeedXor { get; set; } = 0;
 
-        public bool IsReady => BackgroundCalculator != null && GameSeedXor != 0;
+        public readonly bool IsReady = true;
 
         public uint Get(UnitPlayer player)
         {
-            if (GameSeedXor != 0)
-            {
-                return (uint)(player.InitSeedHash ^ GameSeedXor);
-            }
-            else if (BackgroundCalculator == null)
-            {
-                var InitSeedHash = player.InitSeedHash;
-                var EndSeedHash = player.EndSeedHash;
+            return (uint) player.InitSeedHash;
+            //if (GameSeedXor != 0)
+            //{
+            //    return (uint)(player.InitSeedHash ^ GameSeedXor);
+            //}
+            //else if (BackgroundCalculator == null)
+            //{
+            //    var InitSeedHash = player.InitSeedHash;
+            //    var EndSeedHash = player.EndSeedHash;
 
-                BackgroundCalculator = new BackgroundWorker();
+            //    BackgroundCalculator = new BackgroundWorker();
 
-                BackgroundCalculator.DoWork += (sender, args) =>
-                {
-                    uint magic = 0x6AC690C5;
-                    uint offset = 666;
+            //    BackgroundCalculator.DoWork += (sender, args) =>
+            //    {
+            //        uint magic = 0x6AC690C5;
+            //        uint offset = 666;
 
-                    uint divisor = 2 << 16 - 1;
-                    uint mod = 0;
+            //        uint divisor = 2 << 16 - 1;
+            //        uint mod = 0;
 
-                    Parallel.For(0, divisor, (trySeed, state) =>
-                    {
-                        var seedResult = ((uint)trySeed * magic + offset) & 0xFFFFFFFF;
+            //        Parallel.For(0, divisor, (trySeed, state) =>
+            //        {
+            //            var seedResult = ((uint)trySeed * magic + offset) & 0xFFFFFFFF;
 
-                        if (seedResult % divisor == EndSeedHash % divisor)
-                        {
-                            mod = (uint)trySeed;
-                            state.Stop();
-                        }
-                    });
+            //            if (seedResult % divisor == EndSeedHash % divisor)
+            //            {
+            //                mod = (uint)trySeed;
+            //                state.Stop();
+            //            }
+            //        });
 
-                    Parallel.For(0, ((long)uint.MaxValue + 1) / divisor - 1, (i, state) =>
-                    {
-                        var trySeed = mod + i * divisor;
-                        var seedResult = ((uint)trySeed * magic + offset) & 0xFFFFFFFF;
+            //        Parallel.For(0, ((long)uint.MaxValue + 1) / divisor - 1, (i, state) =>
+            //        {
+            //            var trySeed = mod + i * divisor;
+            //            var seedResult = ((uint)trySeed * magic + offset) & 0xFFFFFFFF;
 
-                        if (seedResult == EndSeedHash)
-                        {
-                            GameSeedXor = (uint)InitSeedHash ^ (uint)trySeed;
-                            state.Stop();
-                        }
-                    });
+            //            if (seedResult == EndSeedHash)
+            //            {
+            //                GameSeedXor = (uint)InitSeedHash ^ (uint)trySeed;
+            //                state.Stop();
+            //            }
+            //        });
 
-                    BackgroundCalculator.Dispose();
+            //        BackgroundCalculator.Dispose();
 
-                    if (GameSeedXor == 0)
-                    {
-                        _log.Info("Failed to brute force map seed");
-                        BackgroundCalculator = null;
-                    }
-                };
+            //        if (GameSeedXor == 0)
+            //        {
+            //            _log.Info("Failed to brute force map seed");
+            //            BackgroundCalculator = null;
+            //        }
+            //    };
 
-                BackgroundCalculator.RunWorkerAsync();
-            }
+            //    BackgroundCalculator.RunWorkerAsync();
+            //}
 
-            return 0;
+            //return 0;
         }
     }
 }
